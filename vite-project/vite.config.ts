6f043153d4ae434fa458383f9c74551d5259e4f0
Raphaël Goetter <raphael@goetter.fr>
2024-10-07 10:11:23 +0200

    first

---
+++ vite-project/vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.js
+// https://vitejs.dev/guide/build.html
+
+import { defineConfig } from "vite";
+import UnoCSS from "unocss/vite";
+
+export default defineConfig({
+  // config options
+  plugins: [UnoCSS()],
+});
